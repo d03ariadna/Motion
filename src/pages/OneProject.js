4a8ad092b1dbfa6235c77c01f72452ed0ccdf843
d03ariadna <arod_andaira@hotmail.com>
2023-08-10 20:39:55 -0600

    Fix merge

--- src/pages/OneProject.js
+++ src/pages/OneProject.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import CreateButton from "../components/general/CreateButton"
 
